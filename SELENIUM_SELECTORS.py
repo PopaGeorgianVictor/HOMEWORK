e1a03b009c5d37bc298aaf9130e18f61effe6625
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-11-30 11:23:22 +0200

    enjoy coding

--- SELENIUM_SELECTORS.py
+++ SELENIUM_SELECTORS.py
@@ -65,8 +65,8 @@ driver.get(url)
 # this is for accepting Cookies
 driver.find_element(By.ID, 'ez-accept-all').click()
 
-driver.find_element(By.NAME, 'firstname').send_keys('Popa')
-driver.find_element(By.NAME, 'lastname').send_keys('Georgian')
+driver.find_element(By.NAME, 'firstname').send_keys('Georgian')
+driver.find_element(By.NAME, 'lastname').send_keys('Popa')
 driver.find_element(By.NAME, 'profession').click()
 
 
@@ -100,8 +100,8 @@ driver.find_element(By.ID, 'ez-accept-all').click()
 
 tags_list = driver.find_elements(By.TAG_NAME,'input')
 
-tags_list[0].send_keys('Popa')
-tags_list[1].send_keys('Georgian')
+tags_list[0].send_keys('Georgian')
+tags_list[1].send_keys('Popa')
 tags_list[2].click()
 
 # find element By.CLASS_NAME
@@ -110,8 +110,8 @@ driver.get(url1)
 
 class_list = driver.find_elements(By.CLASS_NAME,'form-control')
 
-class_list[0].send_keys('Popa')
-class_list[1].send_keys('Georgian')
+class_list[0].send_keys('Georgian')
+class_list[1].send_keys('Popa')
 class_list[2].send_keys('QA')
 class_list[3].click()
 
@@ -121,10 +121,10 @@ class_list[3].click()
 
 driver.get(url1)
 
-driver.find_element(By.CSS_SELECTOR,'#first-name').send_keys('Popa') # by ID
-driver.find_element(By.CSS_SELECTOR,'.form-control').send_keys('Popa') # by class
+driver.find_element(By.CSS_SELECTOR,'#first-name').send_keys('Geo') # by ID
+driver.find_element(By.CSS_SELECTOR,'.form-control').send_keys('Geo') # by class
 driver.find_element(By.CSS_SELECTOR,'[placeholder="Enter your job title"]').send_keys('QA') # by attribute=value
-driver.find_element(By.CSS_SELECTOR,'*[placeholder*="first name"]').send_keys('Popa') # by attribute=partial_value
+driver.find_element(By.CSS_SELECTOR,'*[placeholder*="first name"]').send_keys('Geo) # by attribute=partial_value
 
 
 '''Find element By.XPATH'''
@@ -133,23 +133,23 @@ driver.get(url1)
 
 # by attribute=value
 
-driver.find_element(By.XPATH, '//input[@id="first-name"]').send_keys('Popa')
-driver.find_element(By.XPATH, '//input[@id="last-name"]').send_keys('Georgian')
+driver.find_element(By.XPATH, '//input[@id="first-name"]').send_keys('Georgian')
+driver.find_element(By.XPATH, '//input[@id="last-name"]').send_keys('Popa')
 driver.find_element(By.XPATH, '//input[@id="job-title"]').send_keys('QA')
 
 # * all elements that comply with the rule
 
-driver.find_element(By.XPATH, '//*[@id="first-name"]').send_keys('Popa')
+driver.find_element(By.XPATH, '//*[@id="first-name"]').send_keys('Geo')
 
 
 select elements from the list
 
-driver.find_element(By.XPATH, '(//input[@class="form-control"])[1]').send_keys('Popa')
+driver.find_element(By.XPATH, '(//input[@class="form-control"])[1]').send_keys('Geo')
 
 
 # with OR, using pipe |
 
-driver.find_element(By.XPATH, '//input[@id="first-name"] | //input[@id="last-name"]').send_keys('Popa')
+driver.find_element(By.XPATH, '//input[@id="first-name"] | //input[@id="last-name"]').send_keys('Geo')
 
 
 # partial text link
@@ -184,8 +184,25 @@ driver.find_element(By.XPATH,
 
 # using function
 
+# input_by_label
 def input_by_label(label, input):
     my_input = driver.find_element(By.XPATH, f'//label[text()="{label}"]/parent::strong/parent::div//input')
     my_input.send_keys(input)
 
-input_by_label('Job title', 'QA')
+
+input_by_label('First name', 'Alain')
+input_by_label('Last name', 'Delone')
+
+
+
+# input_by_placeholder
+
+def input_by_placeholder(placeholder, input):
+    my_input = driver.find_element(By.XPATH,f'//input[@placeholder="{placeholder}"]')
+    my_input.send_keys(input)
+
+input_by_placeholder('Enter your job title', 'QA')
+
+
+
+
