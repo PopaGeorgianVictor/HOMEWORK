89f8eefc52e4f3e769432ae98906a325b65714f6
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-07 20:21:46 +0200

    enjoy coding

---
+++ SELENIUM_TestRunner.py
@@ -0,0 +1,13 @@
+'''
+1. Make a suite that contains your tests from theme 9 + the tests from
+meeting 10 (who have the class). Generate the report
+2. Think of a test class from the pages suggested in theme 8
+- Write at least 3 test functions in a class (as we did in class)
+Use firefox instead of driver (what do you want, how many test functions do you want,
+freestyle to start thinking of some test scenarios yourself).
+
+'''
+
+import HtmlTestRunner
+import unittest
+
