06a57d739261ce0d86e090614725762c6ec97254
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-07 20:36:04 +0200

    enjoy coding

--- SELENIUM_TestRunner.py
+++ SELENIUM_TestRunner.py
@@ -3,7 +3,7 @@
 meeting 10 (who have the class). Generate the report
 2. Think of a test class from the pages suggested in theme 8
 - Write at least 3 test functions in a class (as we did in class)
-Use firefox instead of driver (what do you want, how many test functions do you want,
+Use driver instead of driver (what do you want, how many test functions do you want,
 freestyle to start thinking of some test scenarios yourself).
 
 '''
