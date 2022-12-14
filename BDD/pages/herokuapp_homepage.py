d127c29178c49beb9db051956ffb8557d8f750ea
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-14 14:49:30 +0200

    way 2 automation

--- BDD/pages/herokuapp_homepage.py
+++ BDD/pages/herokuapp_homepage.py
@@ -1,15 +1,17 @@
+
+
+from selenium.webdriver.common.by import By
 from BDD.pages.base_page import Base_page
 
-
 class Home_page(Base_page):
+    FORM_BTN = ()
 
-    FORM_BTN =()
 
-    def navigate_to_homepage(self):
+    def navigate(self):
         self.driver.get()
-    def click_on_form(self):
 
-
+    def adsd(self)
+        self.driver.find_element(*self.FORM_BTN).click()
 
 
 
