e89a7c15dfe5ab01a3772cd76c935b4324368316
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 16:55:01 +0200

    bdd-pom

--- BDD/pages/login_page.py
+++ BDD/pages/login_page.py
@@ -1,9 +1,11 @@
+from selenium.webdriver.common.by import By
 from BDD.pages.base_page import Base_page
+
 class Login_page(Base_page):
 
-    USERNAME = ()
-    PASSWORD = ()
-    LOGIN_BTN = ()
+    USERNAME = (By.ID, 'username')
+    PASSWORD = (By.ID, 'password')
+    LOGIN_BTN = (By.CSS_SELECTOR, '.fa.fa-2x.fa-sign-in')
 
     def complete_field(self):
         self.driver.find_element(*self.USERNAME).send_keys("tomsmith")
