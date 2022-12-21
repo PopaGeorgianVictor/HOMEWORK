55101e05892869e08d0e51d23c6b81b6c01757f4
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 18:22:07 +0200

    bdd-pom

--- BDD/browser.py
+++ BDD/browser.py
@@ -7,7 +7,7 @@ class Browser():
     driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()))
     driver.get("https://the-internet.herokuapp.com/")
     driver.maximize_window()
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(3)
 
 
    def close(self):
