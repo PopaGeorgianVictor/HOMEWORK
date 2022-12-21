275a1b0e621a2a6aef4fa68c321cae3511cae040
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 15:00:29 +0200

    bdd-pom

---
+++ BDD/browser.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+class Browser():
+    driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()))
+    driver.get("https://the-internet.herokuapp.com/")
+    driver.maximize_window()
+    driver.implicitly_wait(2)
+
+
+   def close(self):
+        self.driver.delete_all_cookies()
+        self.driver.quit()
---
+++ BDD/pages/base_page.py
@@ -0,0 +1,3 @@
+
+class Base_page(Browser):
+    pass
