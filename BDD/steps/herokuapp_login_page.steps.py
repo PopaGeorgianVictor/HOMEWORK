72b18359dcd583b05e27750ea01c331363ed73c4
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 18:04:47 +0200

    bdd-pom

--- BDD/steps/herokuapp_login_page.steps.py
+++ BDD/steps/herokuapp_login_page.steps.py
@@ -3,3 +3,7 @@ from behave import *
 @when("I enter correct username and password")
 def step_impl(context):
     context.login_page_object.complete_field()
+
+@when("I clicked on Login button")
+def step_impl(context):
+    context.login_page_object.login()
