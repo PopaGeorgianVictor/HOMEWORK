0f837b4f3bb9a324c853b027c0c5615859de0478
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 14:48:13 +0200

    bdd-pom

---
+++ BDD/pages/login_page.py
@@ -0,0 +1,13 @@
+from BDD.pages.base_page import Base_page
+class Login_page(Base_page):
+
+    USERNAME = ()
+    PASSWORD = ()
+    LOGIN_BTN = ()
+
+    def complete_field(self):
+        self.driver.find_element(*self.USERNAME).send_keys("tomsmith")
+        self.driver.find_element(*self.PASSWORD).send_keys("SuperSecretPassword!")
+
+    def login(self):
+        self.driver.find_element(*self.LOGIN_BTN).click()
