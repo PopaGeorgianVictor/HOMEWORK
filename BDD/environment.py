7d499ae13f9d702711accb8e1df67b502f608895
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 15:06:44 +0200

    bdd-pom

--- BDD/environment.py
+++ BDD/environment.py
@@ -6,7 +6,7 @@ from browser import Browser
 def before_all(context):
     context.browser = Browser()
     context.home_page_object = Home_page()
-    context.login_page =
+    context.login_page = Login_page()
 
 def after_all(context):
     context.browser.close()
