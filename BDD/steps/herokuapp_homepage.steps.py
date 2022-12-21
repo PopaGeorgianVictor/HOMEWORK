13247993ef38aa87639a664a3054f0dda8c8f881
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-21 15:14:23 +0200

    bdd-pom

--- BDD/steps/herokuapp_homepage.steps.py
+++ BDD/steps/herokuapp_homepage.steps.py
@@ -4,3 +4,7 @@ from behave import *
 @given("I am on herokuapp homepage")
 def step_impl(context):
     context.home_page_object.navigate_to_page()
+
+@when("I click on Form Authentication")
+def step_impl(context):
+    context.home_page_object.click_on_form()
