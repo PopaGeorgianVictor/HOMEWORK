f62f03addc84f0eab4f99ef2ee5852d61fb9e234
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 09:52:53 +0200

    bdd-pom

--- BDD/steps/logout_page.steps.py
+++ BDD/steps/logout_page.steps.py
@@ -3,3 +3,8 @@ from behave import *
 @when('I clicked on Logout button')
 def step_impl(context):
     context.logout_page_object.logout()
+
+@then("I successfully logout and redirect to the '/login' page and info message ' You logged out of the secure area!' is displayed in a green tab")
+def step_impl(context):
+    context.logout_page_object.redirect_to_login_page()
+    context.logout_page_object.info_msg()
