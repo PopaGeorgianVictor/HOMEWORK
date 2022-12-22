4b604899dd02f3313add50cb37acfc48beafb8e6
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 09:40:31 +0200

    bdd-pom

--- BDD/pages/logout_page.py
+++ BDD/pages/logout_page.py
@@ -3,7 +3,6 @@ from BDD.pages.base_page import Base_page
 from selenium.common import NoSuchElementException
 
 
-
 class Logout_page(Base_page):
 
     LOGOUT_BTN = (By.CSS_SELECTOR, '.icon-2x.icon-signout')
