c8f161e72d4e0818e4134100cf2e0a6beb04c8a7
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 09:34:28 +0200

    bdd-pom

--- BDD/pages/logout_page.py
+++ BDD/pages/logout_page.py
@@ -7,6 +7,14 @@ from selenium.common import NoSuchElementException
 class Logout_page(Base_page):
 
     LOGOUT_BTN = (By.CSS_SELECTOR, '.icon-2x.icon-signout')
-
-    def click_on_logout(self):
+    LOGIN_BTN = (By.CSS_SELECTOR, '.fa.fa-2x.fa-sign-in')
+    def logout(self):
         self.driver.find_element(*self.LOGOUT_BTN).click()
+
+    def redirect_to_login_page(self):
+        try:
+            self.driver.find_element(*self.LOGIN_BTN)
+            print('Element exist')
+
+        except NoSuchElementException:
+            print("Element does not exist")
