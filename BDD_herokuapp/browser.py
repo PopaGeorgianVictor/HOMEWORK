1fbec3be30c604cec4e2107723dec5d79583800e
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-19 11:05:15 +0200

    bdd-pom

--- BDD_herokuapp/browser.py
+++ BDD_herokuapp/browser.py
@@ -5,7 +5,7 @@ from selenium.webdriver.firefox.service import Service
 
 class Browser:
 
-    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     driver.maximize_window()
     driver.implicitly_wait(3)
 
